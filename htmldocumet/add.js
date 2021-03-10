23c8dea59d434bcdb3a3b607b0f0d8a93d8d0e59
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-03-10 15:02:59 -0600

    htmldocument

---
+++ htmldocumet/add.js
@@ -0,0 +1,9 @@
+let prompt=require('prompt-sync')();
+//prompt.start();
+let m =prompt("enter number:");
+console.log(typeof m);
+let n=prompt("enter the seconed number:")
+
+let u=parseInt(m)+parseInt(m);
+console.log(u);
+console.log(typeof u);
